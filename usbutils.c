63c7bccb8d179b402558b0c2132436daad16b0e0
Con Kolivas <kernel@kolivas.org>
2013-05-21 10:08:16 +1000

    Provide a usb_ftdi_err function for detecting an ftdi serial error.

--- usbutils.c
+++ usbutils.c
@@ -2048,10 +2048,32 @@ int usb_ftdi_cts(struct cgpu_info *cgpu)
 	unsigned char buf[2], ret;
 
 	libusb_control_transfer(usbdev->handle, (uint8_t)FTDI_TYPE_IN,
-				      (uint8_t)5, (uint16_t)0, (uint16_t)0,
-				      buf, 2, DEVTIMEOUT);
+				(uint8_t)5, (uint16_t)0, (uint16_t)0, buf, 2,
+				DEVTIMEOUT);
 	ret = buf[0] & FTDI_STATUS_B0_MASK;
 	return (ret & FTDI_RS0_CTS);
+}
+
+#define FTDI_RS_DR	1
+#define FTDI_RS_OE	(1<<1)
+#define FTDI_RS_PE	(1<<2)
+#define FTDI_RS_FE	(1<<3)
+#define FTDI_RS_BI	(1<<4)
+#define FTDI_RS_THRE	(1<<5)
+#define FTDI_RS_TEMT	(1<<6)
+#define FTDI_RS_FIFO	(1<<7)
+
+#define FTDI_RS_ERR_MASK (FTDI_RS_BI | FTDI_RS_PE | FTDI_RS_FE | FTDI_RS_OE)
+
+int usb_ftdi_err(struct cgpu_info *cgpu)
+{
+	struct cg_usb_device *usbdev = cgpu->usbdev;
+	unsigned char buf[2];
+
+	libusb_control_transfer(usbdev->handle, (uint8_t)FTDI_TYPE_IN,
+				      (uint8_t)5, (uint16_t)0, (uint16_t)0,
+				      buf, 2, DEVTIMEOUT);
+	return (buf[1] & FTDI_RS_ERR_MASK);
 }
 
 int _usb_write(struct cgpu_info *cgpu, int ep, char *buf, size_t bufsiz, int *processed, unsigned int timeout, enum usb_cmds cmd)
