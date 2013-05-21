3049d88bc3599634bc2a1eca9d33ec31208e2a41
Con Kolivas <kernel@kolivas.org>
2013-05-21 16:42:19 +1000

    Only nmsleep in ftdi _usb_read if the buffer is unlikely to be filled.

--- usbutils.c
+++ usbutils.c
@@ -1950,7 +1950,7 @@ int _usb_read(struct cgpu_info *cgpu, int ep, char *buf, size_t bufsiz, int *pro
 
 			timeout = initial_timeout - (done * 1000);
 			/* The buffer is only filled every 16ms on ftdi */
-			if (ftdi && bufleft > 0)
+			if (ftdi && bufleft > 0 && bufleft < 63)
 				nmsleep(16);
 		}
 
