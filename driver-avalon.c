ae8f25aad7d9ff1d2ecd2d8f11b9600fd44ebbe0
ckolivas <kernel@kolivas.org>
2013-05-22 16:49:19 +1000

    Remove read mutex locking around avalon init.

--- driver-avalon.c
+++ driver-avalon.c
@@ -720,12 +720,6 @@ static void *avalon_get_results(void *userdata)
 	struct avalon_info *info = avalon_infos[avalon->device_id];
 	const int rsize = 512;
 
-	/* This lock prevents the reads from starting till avalon_prepare
-	 * releses it */
-	mutex_lock(&info->read_mutex);
-	info->offset = 0;
-	mutex_unlock(&info->read_mutex);
-
 	while (42) {
 		int amount, err;
 		char buf[rsize];
@@ -777,7 +771,6 @@ static bool avalon_prepare(struct thr_info *thr)
 		quit(1, "Failed to calloc avalon works in avalon_prepare");
 
 	mutex_init(&info->read_mutex);
-	mutex_lock(&info->read_mutex);
 	if (unlikely(pthread_cond_init(&info->read_cond, NULL)))
 		quit(1, "Failed to pthread_cond_init avalon read_cond");
 
@@ -785,7 +778,6 @@ static bool avalon_prepare(struct thr_info *thr)
 		quit(1, "Failed to create avalon read_thr");
 
 	avalon_init(avalon);
-	mutex_unlock(&info->read_mutex);
 
 	cgtime(&now);
 	get_datestamp(avalon->init, &now);
